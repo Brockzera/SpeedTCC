715264a53bedca7c57e92bda790ce633fcbe25b4
Oscar Broch <brochj@gmail.com>
2019-09-18 23:27:38 -0300

    Refactoring

--- format_frame.py
+++ format_frame.py
@@ -32,11 +32,11 @@ def apply_roi(frame, resize_ratio):
     # triângulo lado esquerdo
     pts3 = np.array([[0, r(620)], [r(270), 0], [0, 0]], np.int32)
     cv2.fillPoly(frame, [pts3], color.BLACK)
-    # Linha entre faixas 1 e 2
+    # Line between lane 1 and 2
     pts1 = np.array([[r(480), r(1080)], [r(560), r(0)],
                      [r(640), r(0)], [r(570), r(1080)]], np.int32)
     cv2.fillPoly(frame, [pts1], color.BLACK)
-    # Linha entre faixas 2 e 3
+    # Line between lane 3 and 3
     pts7 = np.array([[r(1310), r(1080)], [r(900), r(0)],
                      [r(990), r(0)], [r(1410), r(1080)]], np.int32)
     cv2.fillPoly(frame, [pts7], color.BLACK)
